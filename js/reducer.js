1b4168edec371fb367257ef7b9ee38c22a6bffe0
DKendle <dina.kendle@gmail.com>
2021-09-26 10:39:39 -0400

    commit tree e275674b70a83b1447f0b22da885b23b8abf9b1c

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,19 @@
 // add code snippets from README
+let state;
+
+function changeState(state = {count: 0}, action){
+    switch(action.type){
+        case 'INCREASE_COUNT': return {count: state.count + 1}
+        defualt: state
+    }
+}
+
+function render(){
+    document.body.textContent = state.count
+}
+
+function dispatch(action){
+    state = changeState(state, action)
+    render()
+}
+
